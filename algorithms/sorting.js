bd17b007e2da7fa3012ce4d0dc9fe63ccfe4a1ca
Tomas Tulka <tomas.tulka@gmail.com>
2019-09-12 06:54:52 +0200

    bubblesort fixed

--- algorithms/sorting.js
+++ algorithms/sorting.js
@@ -1,11 +1,11 @@
 function bubblesort(arr) {
 	const a = [...arr];
 	for (let i = 0; i < a.length - 1; i ++) {
-		for (let j = i + 1; j < a.length; j ++) {
-			if (a[i] > a[j]) {
-				const v = a[i];
-				a[i] = a[j];
-				a[j] = v;
+		for (let j = 0; j < a.length - i; j ++) {
+			if (a[j] > a[j + 1]) {
+				const v = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = v;
 			}
 		}
 	}
@@ -40,6 +40,7 @@ assertEquals('1,2', insertionsort([1,2]).toString());
 assertEquals('1,2', insertionsort([2,1]).toString());
 assertEquals('1,2,3,4,5', insertionsort([5,4,3,2,1]).toString());
 assertEquals('1,2,3,4,5', insertionsort([4,3,1,2,5]).toString());
+
 
 function assertEquals(expected, actual) {
 	if (expected !== actual) {
