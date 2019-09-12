046ebd8219e716c88865e231d4851f2f32f3f848
Tomas Tulka <tomas.tulka@gmail.com>
2019-09-12 06:45:59 +0200

    insertionsort

--- algorithms/sorting.js
+++ algorithms/sorting.js
@@ -12,12 +12,34 @@ function bubblesort(arr) {
 	return a;
 }
 
-assertEquals(bubblesort([]).toString(), '');
-assertEquals(bubblesort([1]).toString(), '1');
-assertEquals(bubblesort([1,2]).toString(), '1,2');
-assertEquals(bubblesort([2,1]).toString(), '1,2');
-assertEquals(bubblesort([5,4,3,2,1]).toString(), '1,2,3,4,5');
-assertEquals(bubblesort([4,3,1,2,5]).toString(), '1,2,3,4,5');
+assertEquals('', bubblesort([]).toString());
+assertEquals('1', bubblesort([1]).toString());
+assertEquals('1,2', bubblesort([1,2]).toString());
+assertEquals('1,2', bubblesort([2,1]).toString());
+assertEquals('1,2,3,4,5', bubblesort([5,4,3,2,1]).toString());
+assertEquals('1,2,3,4,5', bubblesort([4,3,1,2,5]).toString());
+
+function insertionsort(arr) {
+	const a = [...arr];
+	for (let i = 1; i < a.length; i++) {
+		for (let j = 0; j < i; j ++) {
+			if (a[i] < a[j]) {
+				const v = a[i];
+				a[i] = a[j];
+				a[j] = v;
+			}
+		}
+	}
+	
+	return a;
+}
+
+assertEquals('', insertionsort([]).toString());
+assertEquals('1', insertionsort([1]).toString());
+assertEquals('1,2', insertionsort([1,2]).toString());
+assertEquals('1,2', insertionsort([2,1]).toString());
+assertEquals('1,2,3,4,5', insertionsort([5,4,3,2,1]).toString());
+assertEquals('1,2,3,4,5', insertionsort([4,3,1,2,5]).toString());
 
 function assertEquals(expected, actual) {
 	if (expected !== actual) {
