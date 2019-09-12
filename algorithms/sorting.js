42b0793c27a67a15597a57ee1554c38003a9bbfa
Tomas Tulka <tomas.tulka@gmail.com>
2019-09-12 07:45:07 +0200

    optimized

--- algorithms/sorting.js
+++ algorithms/sorting.js
@@ -1,7 +1,7 @@
 function bubblesort(arr) {
 	const a = [...arr];
 	for (let i = 0; i < a.length - 1; i ++) {
-		for (let j = 0; j < a.length - i; j ++) {
+		for (let j = 0; j < a.length - i - 1; j ++) {
 			if (a[j] > a[j + 1]) {
 				const v = a[j];
 				a[j] = a[j + 1];
@@ -21,16 +21,15 @@ assertEquals('1,2,3,4,5', bubblesort([4,3,1,2,5]).toString());
 
 function insertionsort(arr) {
 	const a = [...arr];
-	for (let i = 1; i < a.length; i++) {
-		for (let j = 0; j < i; j ++) {
-			if (a[i] < a[j]) {
-				const v = a[i];
-				a[i] = a[j];
-				a[j] = v;
+	for (let i = 0; i < a.length; i++) {
+		for (let j = i; j > 0; j--) {
+			if (a[j] < a[j - 1]) {
+				const v = a[j];
+				a[j] = a[j - 1];
+				a[j - 1] = v;
 			}
 		}
-	}
-	
+	}	
 	return a;
 }
 
@@ -55,8 +54,7 @@ function selectionsort(arr) {
 			a[i] = a[min];
 			a[min] = v;
 		}
-	}
-	
+	}	
 	return a;
 }
 
@@ -66,6 +64,24 @@ assertEquals('1,2', selectionsort([1,2]).toString());
 assertEquals('1,2', selectionsort([2,1]).toString());
 assertEquals('1,2,3,4,5', selectionsort([5,4,3,2,1]).toString());
 assertEquals('1,2,3,4,5', selectionsort([4,3,1,2,5]).toString());
+
+let c = 0;
+
+function mergesort(a) {
+	if (a.length < 2) {
+		return a;
+	}
+	const pivot = Math.floor(a.length / 2);
+	
+	return insertionsort([...mergesort(a.slice(0, pivot)), ...mergesort(a.slice(pivot))]);
+}
+
+assertEquals('', mergesort([]).toString());
+assertEquals('1', mergesort([1]).toString());
+assertEquals('1,2', mergesort([1,2]).toString());
+assertEquals('1,2', mergesort([2,1]).toString());
+assertEquals('1,2,3,4,5', mergesort([5,4,3,2,1]).toString());
+assertEquals('1,2,3,4,5', mergesort([4,3,1,2,5]).toString());
 
 function assertEquals(expected, actual) {
 	if (expected !== actual) {
