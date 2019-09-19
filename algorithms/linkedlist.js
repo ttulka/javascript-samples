eddd8a6a1ff8b6006ca4827f8d6904ccab538e39
Tomas Tulka <tomas.tulka@gmail.com>
2019-09-19 07:35:03 +0200

    circularNode added

--- algorithms/linkedlist.js
+++ algorithms/linkedlist.js
@@ -39,6 +39,22 @@ function isPalindrom(list) {
 	return true;
 }
 
+function circularNode(list) {
+	if (!list.head || !list.head.next) {
+		return null;
+	}
+	let slow = list.head;
+	let fast = list.head.next.next;
+	while (fast && fast.next) {
+		if (slow && slow === fast) {
+			return slow;
+		}
+		fast = fast.next.next;
+		slow = slow.next;
+	}
+	return null;
+}
+
 assertEquals('1,2,3', new LinkedList(1,2,3).asArray().toString());
 
 assertEquals(true, isPalindrom(new LinkedList(1)));
@@ -49,6 +65,17 @@ assertEquals(true, isPalindrom(new LinkedList(1,2,3,2,1)));
 assertEquals(true, isPalindrom(new LinkedList(1,2,3,3,2,1)));
 assertEquals(false, isPalindrom(new LinkedList(1,2)));
 assertEquals(false, isPalindrom(new LinkedList(1,2,3)));
+
+assertEquals(null, circularNode(new LinkedList(1)));
+assertEquals(null, circularNode(new LinkedList(1,1)));
+assertEquals(null, circularNode(new LinkedList(1,2,3)));
+
+const circularList = new LinkedList(1);
+const node4 = new Node(4);
+const node2 = new Node(2, new Node(3, node4));
+node4.next = node2;
+circularList.head.next = node2;
+assertEquals(2, circularNode(circularList).data);
 
 function assertEquals(expected, actual) {
 	console.assert(expected === actual, 'Expected ' + expected + ', got ' + actual);
