e3fa28be552d6d2d9f6ac07c2acc1fcb5e6cb27d
Tomas Tulka <tomas.tulka@gmail.com>
2019-09-22 08:12:46 +0200

    flip to win

--- algorithms/binary-numbers.js
+++ algorithms/binary-numbers.js
@@ -11,12 +11,52 @@ function asBinary(num) {
 	return res;
 }
 
+// Flip Bit to Win: You have an integer and you can flip exactly one bit from a O to a 1. 
+// Find the length of the longest sequence of 1 s you could create.
+function longestSequenceOf1s(num) {
+	let prev = 0;
+	let curr = 0;
+	let max = 0;
+		
+	let i = 0;
+	let prevBit = 0;
+	while (num >> i > 0) {
+		const currBit = num & 1 << i;
+		i++;
+		
+		if (currBit) {
+			curr++;
+		}		
+		if (prevBit && !currBit || !(num >> (i + 1))) {
+			if (max < curr + prev) {
+				max = curr + prev + (curr && prev ? 1 : 0);
+			}			
+			prev = curr;
+			curr = 0;
+		}
+		if (!currBit) {			
+			curr = 0;		
+		}
+		if (!prevBit && !currBit) {
+			prev = 0;
+		}		
+		prevBit = currBit;		
+	}
+	return max;
+}
+
+assertEquals('', asBinary(0).toString());
 assertEquals('', asBinary(0).toString());
 assertEquals('1', asBinary(1).toString());
 assertEquals('1,0', asBinary(2).toString());
 assertEquals('1,0,1', asBinary(5).toString());
 assertEquals('1,0,0,0', asBinary(8).toString());
 assertEquals('1,0,0,0,1', asBinary(17).toString());
+
+assertEquals(8, longestSequenceOf1s(0b11011101111));
+assertEquals(5, longestSequenceOf1s(0b01011100111));
+assertEquals(6, longestSequenceOf1s(0b11000111111));
+assertEquals(7, longestSequenceOf1s(0b01111101100));
 
 function assertEquals(expected, actual) {
 	console.assert(expected === actual, 'Expected ' + expected + ', got ' + actual);
